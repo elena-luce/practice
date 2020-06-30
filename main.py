0df1004dcdd320b592b07d9f16bc89eade736104
elena-luce <hdopira@gmail.com>
2020-06-30 23:17:50 +0300

    Нормализовали по-умолчанию

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import csv
 import pandas as pd
 import numpy as np
 
@@ -12,8 +13,16 @@ data = np.array(df)
 for i in range(len(data)):
     
     y = data[i][0]
-    if pd.isna(y):
-        y=3000
-    else:
-        y = int(y)
-    print(y)
+    # if pd.isna(y):
+    #     y=3000
+    # else:
+    #     y = int(y)
+    # print(y)
+
+    for item in range(1,7):
+        if pd.isna(data[i][item]):
+             data[i][item] = data[i][7]/6
+
+with open('Norm.csv', "w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerows(data)
