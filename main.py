915e9131a0689246b7a8744eed7ed711798fd417
elena-luce <hdopira@gmail.com>
2020-06-30 22:12:44 +0300

    #10 Замена nan на 3000 в столбце year

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-import pandas
+import pandas as pd
+import numpy as np
 
 
-df = pandas.read_csv('NFA 2018.csv',index_col='country')
-# print(df)
-df.to_csv('New.csv')
+df = pd.read_csv('NFA 2018.csv',index_col='country')
+data = np.array(df)
+
+
+# df.to_csv('New.csv')
+
+
+for i in range(len(data)):
+    
+    y = data[i][0]
+    if pd.isna(y):
+        y=3000
+    else:
+        y = int(y)
+    print(y)
