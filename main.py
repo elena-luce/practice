da63018e1cd657299690503326e9dfb9cc204ffd
elena-luce <hdopira@gmail.com>
2020-07-01 15:46:34 +0300

    #2 Заменили пустое значение на заданное. Правильно занесли столбцы

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import pandas as pd
 import numpy as np
 
 
-df = pd.read_csv('NFA 2018.csv',index_col='country')
+df = pd.read_csv('NFA 2018.csv')
+
+headers = df.columns.tolist()
 data = np.array(df)
 
 
@@ -11,18 +13,19 @@ data = np.array(df)
 
 
 for i in range(len(data)):
-    
-    y = data[i][0]
+
+    y = data[i][1]
     # if pd.isna(y):
     #     y=3000
     # else:
     #     y = int(y)
     # print(y)
     
-    for item in range(1,7):
+    for item in range(2,8):
         if pd.isna(data[i][item]):
-             data[i][item] = data[i][7]/6
+             data[i][item] = data[i][8]/6
 
 with open('Norm.csv', "w", newline="") as file:
     writer = csv.writer(file)
+    writer.writerow(headers)
     writer.writerows(data)
